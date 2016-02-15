00cddfa382e72171f10ca789c0b4b22e604e90c0
Yoshiki Makino <ymakino_@hotmail.com>
2016-02-15 11:12:26 +0900

    Add identification mode to HardwareAddressPropertyDelegate which is useful for creating a data for EPC.x83.

--- src/humming/sample/HardwareAddressPropertyDelegate.java
+++ src/humming/sample/HardwareAddressPropertyDelegate.java
@@ -8,6 +8,7 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,6 +22,10 @@ public class HardwareAddressPropertyDelegate extends PropertyDelegate {
     
     private NetworkInterface networkInterface;
     private boolean isStringMode;
+    private boolean isIdentificationMode;
+    
+    private byte[] dataTemplate;
+    private int dataOffset;
 
     public HardwareAddressPropertyDelegate(EPC epc, boolean getEnabled, boolean setEnabled, boolean notifyEnabled) {
         super(epc, getEnabled, setEnabled, notifyEnabled);
@@ -28,6 +33,8 @@ public class HardwareAddressPropertyDelegate extends PropertyDelegate {
         
         networkInterface = null;
         isStringMode = false;
+        dataTemplate = new byte[]{(byte)0xFE,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+        dataOffset = 1;
         
         LOGGER.exiting(CLASS_NAME, "HardwareAddressPropertyDelegate");
     }
@@ -104,59 +111,72 @@ public class HardwareAddressPropertyDelegate extends PropertyDelegate {
         return isStringMode;
     }
     
-    private byte[] getHardwareAddressRaw() {
-        LOGGER.entering(CLASS_NAME, "getHardwareAddressRaw");
+    public void setIdentificationMode(boolean isIdentificationMode) {
+        this.isIdentificationMode = isIdentificationMode;
+    }
+    
+    public boolean isIdentificationMode() {
+        return isIdentificationMode;
+    }
+    
+    private byte[] getHardwareAddress() {
+        LOGGER.entering(CLASS_NAME, "getHardwareAddress");
         
         byte[] haddr = null;
         
         try {
             haddr = networkInterface.getHardwareAddress();
             if (haddr == null) {
-                LOGGER.logp(Level.WARNING, CLASS_NAME, "getHardwareAddressRaw", "cannot get hardware address: " + networkInterface);
+                LOGGER.logp(Level.WARNING, CLASS_NAME, "getHardwareAddress", "cannot get hardware address: " + networkInterface);
             }
         } catch (SocketException ex) {
-            LOGGER.logp(Level.WARNING, CLASS_NAME, "getHardwareAddressRaw", "cannot get hardware address: ", ex);
+            LOGGER.logp(Level.WARNING, CLASS_NAME, "getHardwareAddress", "cannot get hardware address: ", ex);
         }
         
-        LOGGER.exiting(CLASS_NAME, "getHardwareAddressRaw", haddr);
+        LOGGER.exiting(CLASS_NAME, "getHardwareAddress", haddr);
         return haddr;
     }
     
-    private byte[] getHardwareAddressString() {
-        LOGGER.entering(CLASS_NAME, "getHardwareAddressString");
-        
-        byte[] haddr = getHardwareAddressRaw();
-        
-        if (haddr == null) {
-            LOGGER.exiting(CLASS_NAME, "getHardwareAddressString", null);
-            return null;
-        }
-        
+    private String bytesToString(byte[] bytes) {
         StringBuilder builder = new StringBuilder();
-        for (int i=0; i<haddr.length; i++) {
-            builder.append(String.format("%02x", 0x00ff & haddr[i]));
+        for (int i=0; i<bytes.length; i++) {
+            builder.append(String.format("%02x", 0x00ff & bytes[i]));
         }
         
-        byte[] bytes = builder.toString().getBytes();
-        LOGGER.exiting(CLASS_NAME, "getHardwareAddressString", bytes);
+        return builder.toString();
+    }
+    
+    private byte[] makeStringBytes(byte[] haddr) {
+        return bytesToString(haddr).getBytes();
+    }
+    
+    private byte[] makeIdentificationBytes(byte[] haddr) {
+        byte[] bytes = Arrays.copyOf(dataTemplate, dataTemplate.length);
+        
+        int offset = Math.max(dataOffset, bytes.length - haddr.length);
+        int length = Math.min(bytes.length - dataOffset, haddr.length);
+        System.arraycopy(haddr, 0, bytes, offset, length);
+        
         return bytes;
     }
     
     public ObjectData getHardwareAddressData() {
         LOGGER.entering(CLASS_NAME, "getHardwareAddressData");
         
-        byte[] bytes = null;
-        
-        if (isStringMode) {
-            bytes = getHardwareAddressString();
-        } else {
-            bytes = getHardwareAddressRaw();
-        }
+        byte[] bytes = getHardwareAddress();
         
         if (bytes == null) {
             LOGGER.logp(Level.WARNING, CLASS_NAME, "getHardwareAddressData", "cannot get hardware address: " + networkInterface);
             LOGGER.exiting(CLASS_NAME, "getHardwareAddressData", null);
             return null;
+        }
+        
+        if (isIdentificationMode) {
+            bytes = makeIdentificationBytes(bytes);
+        }
+        
+        if (isStringMode) {
+            bytes = makeStringBytes(bytes);
         }
         
         ObjectData objectData = new ObjectData(bytes);
